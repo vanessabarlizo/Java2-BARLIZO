d46157ee6e86fdd50689bfbf246d81f7b88907bf
vanessabarlizo <59692862+vanessabarlizo@users.noreply.github.com>
2020-01-13 14:45:09 +0800

    Add files via upload

---
+++ MidtermAct04.java
@@ -0,0 +1,41 @@
+import java.io.File; 
+import java.util.Scanner; 
+import java.io.*;
+
+public class MidtermAct04
+{
+	public static void main(String[] args) {
+		try {
+			File file = new File("Phonebook.txt");
+			Scanner sc = new Scanner(file);
+			String data = "";
+
+			while (sc.hasNext()){
+				data = sc.next();
+				if(data.equals("1001-11")) {
+					System.out.print("We've Found the Record!");
+					break;
+				}
+			}
+			sc.close();
+
+			PrintWriter out = new PrintWriter ("Record_found.txt");
+			Scanner in = new Scanner(file);
+			while (in.hasNext()){
+				String str = in.next();
+				if(str.equals("1001-11")) {
+					System.out.println("Code: " + str);
+					System.out.println("First_Name: " + in.next());
+					System.out.println("Last_Name: " + in.next());
+					System.out.println("CP #: " + in.next());
+					break;
+				}
+			}
+			out.close();
+			in.close();
+
+		} catch (IOException e) {
+            System.out.println("Exception");
+        }
+	}
+}
